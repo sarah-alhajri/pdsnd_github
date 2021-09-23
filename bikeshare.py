f5e0627f6439e9f6c28ecdd28f5901b3b51438a5
sarah saeed <sosohhhhhhhh124@gmail.com>
2021-09-24 00:03:17 +0300

    Add a goodbye message to the user

--- bikeshare.py
+++ bikeshare.py
@@ -201,6 +201,7 @@ def main():
         display_data(df)
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
+            print("goodbye *_*")
             break
 
 
